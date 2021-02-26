fc59478e97db9b4ee28872b047f731aa4b2aa0ab
Kautenja <ckfour@icloud.com>
2021-02-26 09:34:13 -0600

    remove unnecessary dataset

--- dataset/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A generalization of a semantic segmentation DataSet."""
-from ._dataset import DataSet
-
-
-# explicitly define the outward facing API of this module
-__all__ = [DataSet.__name__]
